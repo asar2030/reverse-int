e3167e33db0b10796bda151c86421bb8926756be
Asar Akhmetbekov <asarakhmetbekov@MacBook-Pro-Asar.local>
2021-08-13 22:08:10 +0600

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let string = n.toString()
+    if (n < 0) {
+        string = string.slice(1)
+    }
+    return +string.split('').reverse().join('')
 }
